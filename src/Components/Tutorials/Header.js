c3d46b14f9af87b012a85c775c47dee868e0b3d7
Trey <treytiller@outlook.com>
2019-11-08 08:13:01 -0500

    Logo added and sized to Tutorials Component

--- src/Components/Tutorials/Header.js
+++ src/Components/Tutorials/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../../src/HomeBrewed-logo.png';
+import './Header.css';
 
 class Header extends React.Component {
     render() {
         return(
             <header>
-                <h1>HomeBrewed</h1>
-                <Link to="/login">Skip</Link>
+                <img src={ Logo } alt="logo" />
+                <Link to="/login"><button>Skip</button></Link>
             </header>
         )
     }
