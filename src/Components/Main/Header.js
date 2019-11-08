51726c3722d55693bab959f8df101c5b8c04e7c6
Trey <treytiller@outlook.com>
2019-11-08 08:01:06 -0500

    Logo added--- testing

--- src/Components/Main/Header.js
+++ src/Components/Main/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../../src/HomeBrewed-logo.png';
 
 class Header extends React.Component {
     render() {
         return(
             <header>
                 <Link to="/login">Log Out</Link>
-                <h1>HomeBrewed</h1>
+                <img src={Logo} alt="logo" />
                 <Link to="/add-brew">Add New Brew</Link>
             </header>
         )
