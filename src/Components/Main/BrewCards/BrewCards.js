3b7cadfcbd1038de18bc8ddcd75010533381017a
Trey <treytiller@outlook.com>
2019-11-16 13:03:39 -0500

    Reverted back to previous local state for assignment purposes

--- src/Components/Main/BrewCards/BrewCards.js
+++ src/Components/Main/BrewCards/BrewCards.js
@@ -7,19 +7,46 @@ class Cards extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            "listData": []
+            "listData": [
+                {
+                    "title": "Clever",
+                    "skill": "Medium",
+                    "time": "5:00"
+                },
+                {
+                    "title": "French Press",
+                    "skill": "Easy",
+                    "time": "4:00"
+                },
+                {
+                    "title": "Chemex",
+                    "skill": "Medium",
+                    "time": "5:00"
+                },
+                {
+                    "title": "Pour Over",
+                    "skill": "Hard",
+                    "time": "6:00"
+                },
+                {
+                    "title": "Cold Brew",
+                    "skill": "Easy",
+                    "time": "12:00:00"
+                },
+
+            ]
         }
     }
 
-    componentDidMount() {
-        fetch("http://localhost:8000/api/recipes")
-            .then(res => res.json())
-            .then(recipes => {
-                this.setState({
-                    listData: recipes
-                })
-            })
-    }
+    // componentDidMount() {
+    //     fetch("http://localhost:8000/api/recipes")
+    //         .then(res => res.json())
+    //         .then(recipes => {
+    //             this.setState({
+    //                 listData: recipes
+    //             })
+    //         })
+    // }
 
     render() {
         return (
